f12688c61e82cb2ca148c11a37056beaf45ae063
Procode AS <81738875+procodeas@users.noreply.github.com>
2022-02-05 12:29:18 +0000

    commit

---
+++ nested if.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    System.out.println ("nested statement is apply in age detection");
+    Scanner a = new Scanner (System.in);
+    int b = a.nextInt ();
+    if (b <= 1)
+      {
+	System.out.println ("apply to vote");
+      }
+    else if (b == 0)
+      {
+         System.out.println("you small boy");
+      }
+      else{
+          System.out.println("fuck u r ans");
+      }
+
+  }
+}
