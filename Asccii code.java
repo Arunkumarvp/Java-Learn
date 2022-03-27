01815636682ad03127f760ad319b744a3a256ac0
Procode AS <81738875+procodeas@users.noreply.github.com>
2022-03-27 01:31:50 -0700

    Create Asccii code.java

---
+++ Asccii code.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        int aci=ch;
+        int castAscii =(int) ch;
+        System.out.println(castAscii);
+    }
+}
