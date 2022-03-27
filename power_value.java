ce934fdddaa2e4962d12e3216d96f1377b2864b3
Procode AS <81738875+procodeas@users.noreply.github.com>
2022-03-27 01:06:06 -0700

    Create power_value.java

---
+++ power_value.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner sc=new Scanner(System.in);
+        double result;
+        double sa=sc.nextInt();
+        double sb=sc.nextInt();
+        result=Math.pow(sa,sb);
+     System.out.println(result);
+    }
+}
