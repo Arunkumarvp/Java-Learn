9a2cefa3ac7969f5bd1910d8d9198448f38b6ab4
Procode AS <81738875+procodeas@users.noreply.github.com>
2022-04-05 19:41:33 -0800

    Create octal to decimal conversion.java

---
+++ octal to decimal conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class solve{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String hexa=sc.next();
+        int decimal =Integer.parseInt(hexa,8);
+        System.out.println(decimal);
+    }
+}
