2effc2e423e6d6c2ea64a26b6329a0034196897d
Procode AS <81738875+procodeas@users.noreply.github.com>
2022-04-05 20:09:18 -0800

    Create round_of_float_value.java

---
+++ round_of_float_value.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.Math;
+class solve{
+    public static void main(String args[]){
+        Scanner oc= new Scanner(System.in);
+        float value=oc.nextFloat();
+        System.out.println(String.format("%.6f",value));
+    }
+}
