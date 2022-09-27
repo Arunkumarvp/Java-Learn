210ac67b4c178ac061447b3c9787416892cc5244
Arunkumarvp <arunkumarvp2001@gmail.com>
2022-09-27 15:18:01 +0530

    commit

--- pattern/patter1.java
+++ pattern/patter1.java
@@ -2,16 +2,18 @@ package pattern;
 
 public class patter1 {
     public static void main(String[] args) {
-        int num = 5;
-        for (int i = 0; i < num; i++) {
-            for (int j = num - i; j > 1; j--) {
-                System.out.print(" ");
+        int num = 6;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= ((2 * num) - 1); j++) {
+                if (j >= (num - (i - 1)) && j <= (num + (i - 1))) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.println();
+                }
+
             }
-            for (int n = 0; n <= i; n++) {
-                System.out.print("@ ");
-            }
-            System.out.println();
+
         }
     }
 }
-
